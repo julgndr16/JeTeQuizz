92d388b0350fd9f5cf159c1f22dd9fff1f947354
Vladouh <guillemontvladimir@gmail.com>
2024-05-22 17:47:11 +0200

    add answer line and changes Question.tsx

--- src/ui/components/Question.tsx
+++ src/ui/components/Question.tsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
+
 export default function Question({ questionInfo, onClick }) {
   // state
+  const [reponses, setReponses] = useState([
+    { id: 1, nom: "Réponse 1" },
+    { id: 2, nom: "Réponse 2" },
+    { id: 3, nom: "Réponse 3" },
+  ]);
 
   // comportements
+  const handleDelete = () => {
+    console.log("handleDelete");
+  };
 
   // affichage (render)
   return (
     <li>
-      {questionInfo.nom} <button onClick={onClick}>X</button>
+      {questionInfo.nom}
+      <button onClick={onClick}>X</button>
+      <ul>
+        {reponses.map((reponse) => (
+          <li key={reponse.id}>
+            {reponse.nom}
+            <button onClick={handleDelete}>X</button>
+          </li>
+        ))}
+      </ul>
     </li>
   );
 }
