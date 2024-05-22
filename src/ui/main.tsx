dd1a5ac49f9370f6cde3ee05b3603c674536763c
Vladouh <guillemontvladimir@gmail.com>
2024-05-22 14:26:51 +0200

    minor change to import App

--- src/ui/main.tsx
+++ src/ui/main.tsx
@@ -6,7 +6,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import App from "routes/App";
+import App from "../ui/routes/App";
 
 export type IStore = {
   init: boolean;
