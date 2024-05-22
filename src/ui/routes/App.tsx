2f945222c1a9a21d798baf0007379cbb9e8248eb
gastonduault <gas.duault@gmail.com>
2024-05-22 14:53:18 +0200

    fix import after rebase

--- src/ui/routes/App.tsx
+++ src/ui/routes/App.tsx
@@ -1,12 +1,5 @@
 import { FunctionComponent, useContext, useEffect } from "react";
-<<<<<<< HEAD
-import Header from "./components/Header";
-import Counter from "../components/Counter";
-=======
-import Counter from "./components/Counter";
-import Header from "./components/Header";
-import { Button } from "@mui/material";
->>>>>>> 2f1546d (front of the header)
+import Header from "../components/Header";
 import { store } from "../main";
 
 const App: FunctionComponent = () => {
@@ -30,9 +23,6 @@ const App: FunctionComponent = () => {
       padding: "0 0",
     }}>
       <Header />
-      {/*<h1>Material UI</h1>*/}
-      {/*<Counter default_count={3} />*/}
-      {/*<Button variant={"contained"}>Hello World</Button>*/}
     </div>
   );
 };
